efa68cbb5b3a51808535983837e792e104c16374
Artyom <sheludeshev.artyom@mail.ru>
2022-07-27 23:59:29 +0300

    Started creating form

--- src/components/Plan/SubNavigation/SubNavigation.tsx
+++ src/components/Plan/SubNavigation/SubNavigation.tsx
@@ -1,4 +1,5 @@
 import { AddHexagon } from 'iconoir-react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { create } from '../../../redux/planSlice'
@@ -7,22 +8,30 @@ import styles from './SubNavigation.module.css'
 
 export default function SubNavigation() {
   const dispatch = useDispatch()
+  const [isCreating, setIsCreating] = useState(false)
 
   return (
     <div className={styles.container}>
       <h2>Plan</h2>
       <hr />
-      <Button
-        label='Create new plan'
-        onClick={() => {
-          dispatch(create({
-            title: 'MyFirstGoal',
-            deposit: 200,
-            goal: 100
-          }))
-        }}
-        iconComponent={<AddHexagon height={24} width={24} color='#FFFFFF' />}
-      />
+      { isCreating ? (
+        <div>Creating plan...</div>
+      ) : (
+        <Button
+          label='Create new plan'
+          onClick={() => {
+            dispatch(create({
+              title: 'MyFirstGoal',
+              deposit: 200,
+              goal: 100
+            }))
+
+            setIsCreating(true)
+          }}
+          iconComponent={<AddHexagon height={24} width={24} color='#FFFFFF' />}
+        />
+      )}
+      
       <Outlet />
     </div>
   )
